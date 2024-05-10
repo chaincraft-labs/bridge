53ae80bc3e35b73e9c84035566026e2c110b97a7
ibourn <igor.bournazel@orange.fr>
2024-05-10 22:39:56 +0200

    :truck: renamed token contracts

--- hardhat/contracts/BridgedDai.sol
+++ hardhat/contracts/BridgedDai.sol
@@ -2,17 +2,17 @@
 
 pragma solidity ^0.8.20;
 
-import "./TokenBase.sol";
+import "./BridgedToken.sol";
 
 /**
  * DAI token on Aft tesnet
  */
-contract ADai is TokenBase {
-    event CustomTransfer(address indexed from, address indexed to, uint256 amount);
+contract BridgedDai is BridgedToken {
+    // event CustomTransfer(address indexed from, address indexed to, uint256 amount);
 
     uint256 public constant INITIAL_SUPPLY = 1000000 * 10 ** 18;
 
-    constructor() TokenBase("aDAI Token", "aDAI") {
+    constructor() BridgedToken("DAI Token", "aDAI") {
         _mint(msg.sender, INITIAL_SUPPLY);
     }
 
--- hardhat/contracts/BridgedToken.sol
+++ hardhat/contracts/BridgedToken.sol
@@ -4,6 +4,15 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+/*
+ * @todo: 
+ * - Add Ownable
+ * - batchMint and burn ? (to have a better gas usage for minting and burning)
+ * - Add a way to pause the contract
+ * - Add a way to change the admin (admin should be the bridge contract address)
+ * - ? Factory contract to create new bridged tokens ? (handle special cases)
+ * - remove CustomTransfer event
+ */
 contract BridgedToken is ERC20 {
     address public admin; // bridge contract address
 
